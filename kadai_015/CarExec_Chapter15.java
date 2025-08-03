76d3abeff3d1b6d36d4af8cde109095d7d81fee8
oo <rchan@softbank.ne.jp>
2025-08-03 23:07:30 +0900

    Add files via upload

---
+++ kadai_015/CarExec_Chapter15.java
@@ -0,0 +1,18 @@
+package kadai_015;
+
+public class CarExec_Chapter15 {
+	
+	//車クラスを実行する
+	
+	public void main(String[] args) {
+		
+		//インスタンス化
+		Car_Chapter15 car = new Car_Chapter15();
+		
+		car.changeGear(3);
+		
+		car.run();
+		
+		
+		
+	}
---
+++ kadai_015/Car_Chapter15.java
@@ -0,0 +1,36 @@
+package kadai_015;
+
+public class Car_Chapter15 {
+	
+	//車クラス
+	//初期設定
+	private int gear;
+	private int speed;
+	
+	//コンストラクタ
+	public void Car_chapter15() {
+	}
+	
+	//メソッド1
+	public int changeGear(int afterGear) {
+		switch(afterGear) {
+		case 1 -> this.speed = 10;
+		case 2 -> this.speed = 20;
+		case 3 -> this.speed = 30;
+		case 4 -> this.speed = 40;
+		case 5 -> this.speed = 50;
+		default -> this.speed = 10;
+	}
+		System.out.println("ギア"+ gear + "から" + afterGear + "に切り替えました");
+		this.speed = afterGear;
+		return afterGear;
+		
+	}
+	
+	
+	//メソッド2
+	public void run() {
+		System.out.println("速度は時速" + this.speed + "kmです");		
+	}
+}
+	
