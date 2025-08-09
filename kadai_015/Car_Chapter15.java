fe081af9df81e0ba8dafd81ca2aea569dae3275a
oo <rchan@softbank.ne.jp>
2025-08-10 08:25:57 +0900

    Update Car_Chapter15.java

--- kadai_015/Car_Chapter15.java
+++ kadai_015/Car_Chapter15.java
@@ -1,18 +1,19 @@
-package kadai_015;
+ppackage kadai_015;
 
 public class Car_Chapter15 {
 	
 	//車クラス
 	//初期設定
-	private int gear;
-	private int speed;
+	private int gear=1;
+	private int speed=10;
 	
 	//コンストラクタ
-	public void Car_chapter15() {
+	public void Car_Chapter15() {
 	}
 	
 	//メソッド1
-	public int changeGear(int afterGear) {
+	public void changeGear(int afterGear) {
+
 		switch(afterGear) {
 		case 1 -> this.speed = 10;
 		case 2 -> this.speed = 20;
@@ -21,10 +22,8 @@ public class Car_Chapter15 {
 		case 5 -> this.speed = 50;
 		default -> this.speed = 10;
 	}
-		System.out.println("ギア"+ gear + "から" + afterGear + "に切り替えました");
-		this.speed = afterGear;
-		return afterGear;
-		
+		System.out.println("ギア"+ this.gear + "から"+ afterGear +"に切り替えました");
+		this.gear= afterGear;	
 	}
 	
 	
